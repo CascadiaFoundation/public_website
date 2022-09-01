f6e59aa9fc949a89f29db30550345b33dd72412d
Tony.W <credit.first000@gmail.com>
2022-09-01 14:11:57 -0500

    fix: dropdown menu of header design

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -55,7 +55,7 @@ const Header = (): JSX.Element => {
             <>
               <div className='mx-auto w-full px-10 md:px-20 lg:px-32'>
                 <div className='flex h-16 items-center justify-between'>
-                  <div className='flex h-full items-center'>
+                  <div className='z-10 flex h-full items-center'>
                     <div className='flex-shrink-0'>
                       <Link href='/'>
                         <div className='text-xl font-semibold text-white'>
@@ -71,7 +71,7 @@ const Header = (): JSX.Element => {
                   </div>
                   <div className='-mr-2 flex md:hidden'>
                     {/* Mobile menu button */}
-                    <Disclosure.Button className='inline-flex items-center justify-center rounded-md bg-transparent p-2 text-white hover:text-main-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-1 focus:ring-offset-main-900'>
+                    <Disclosure.Button className='z-10 inline-flex items-center justify-center rounded-md bg-transparent p-2 text-white hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-white'>
                       <span className='sr-only'>Open main menu</span>
                       {open ? (
                         <XMarkIcon
@@ -89,23 +89,21 @@ const Header = (): JSX.Element => {
                 </div>
               </div>
 
-              <Disclosure.Panel className='backdrop-blur-lg md:hidden'>
-                <div className='space-y-1 px-2 pt-2 pb-3 sm:px-3'>
+              <Disclosure.Panel className='absolute inset-x-0 top-0 bg-main-900 md:hidden'>
+                <div className='space-y-1 px-2 pt-14 pb-3 sm:px-3'>
                   {navigation.map((item) => (
                     <Disclosure.Button
                       key={item.name}
-                      as='a'
-                      href={item.href}
                       className={clsx(
-                        'block rounded-md px-3 py-2 text-base font-medium text-secondary-100 hover:bg-gray-700 hover:text-white',
+                        'block w-full rounded-md px-3 py-2 text-base font-medium text-secondary-100 transition-all hover:bg-white hover:text-main-900',
                         pathname === item.href && 'text-white'
                       )}
                     >
-                      {item.name}
+                      <Link href={item.href}>{item.name}</Link>
                     </Disclosure.Button>
                   ))}
                 </div>
-                <div className='border-t border-gray-700 pt-4 pb-3'>
+                <div className='border-t border-secondary-200 pt-4 pb-3'>
                   <div className='flex items-center px-5'>
                     <div className='flex-shrink-0'>
                       <Image
@@ -120,13 +118,13 @@ const Header = (): JSX.Element => {
                       <div className='text-base font-medium leading-none text-white'>
                         {user.name}
                       </div>
-                      <div className='text-sm font-medium leading-none text-gray-400'>
+                      <div className='text-sm font-medium leading-none text-secondary-100'>
                         {user.email}
                       </div>
                     </div>
                     <button
                       type='button'
-                      className='ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                      className='ml-auto flex-shrink-0 rounded-full bg-white p-1 text-main-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
                     >
                       <span className='sr-only'>View notifications</span>
                       <BellIcon className='h-6 w-6' aria-hidden='true' />
