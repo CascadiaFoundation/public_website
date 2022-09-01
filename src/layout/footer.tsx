61e1f4a3a12b909faafbd1f78f723705d14c26c7
Tony.W <credit.first000@gmail.com>
2022-09-01 09:40:43 -0500

    fix: footer-link-hover

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -49,7 +49,9 @@ const Footer = (): JSX.Element => {
     <div className='bg-main-900 p-[5%] pb-0'>
       <div className='grind-cols-2 grid gap-y-8 sm:grid-cols-6 lg:gap-y-0'>
         <div className='grid grid-cols-2 sm:col-span-2 sm:block lg:col-span-1'>
-          <div className='pb-6 text-2xl text-white'>Cascadia</div>
+          <div className='cursor-pointer pb-6 text-2xl text-white'>
+            Cascadia
+          </div>
           <div className='capitaliz flex flex-col gap-1 text-base text-secondary-100'>
             <a
               href='#'
@@ -82,14 +84,16 @@ const Footer = (): JSX.Element => {
             key={item.title}
             className='col-span-2 hidden sm:block lg:col-span-1'
           >
-            <div className='pb-4 text-base text-white'>{item.title}</div>
+            <div className='cursor-pointer pb-4 text-base text-white'>
+              {item.title}
+            </div>
             <ul className='capitaliz text-base text-secondary-100'>
               {item.list.map((li) => (
                 <li
                   key={li}
                   className='py-[2px] text-base font-normal text-secondary-100/80 hover:text-white'
                 >
-                  {li}
+                  <a href='#'>{li}</a>
                 </li>
               ))}
             </ul>
@@ -100,7 +104,7 @@ const Footer = (): JSX.Element => {
             {itemList.map((item, index) => (
               <AccordionItem
                 key={item.title}
-                className='py-2 font-semibold text-white'
+                className='cursor-pointer py-2 font-semibold text-white'
                 title={item.title}
                 collapse={collapsedIndex === index}
                 onClick={() => handleCollapse(index)}
@@ -108,7 +112,7 @@ const Footer = (): JSX.Element => {
                 <ul className='pl-8 text-left text-secondary-100'>
                   {item.list.map((li) => (
                     <li key={li} className='py-2'>
-                      {li}
+                      <a href='#'>{li}</a>
                     </li>
                   ))}
                 </ul>
@@ -118,7 +122,7 @@ const Footer = (): JSX.Element => {
         </div>
       </div>
 
-      <div className='flex flex-col items-center justify-between gap-4 pt-8 pb-6 sm:flex-row sm:gap-0'>
+      <div className='flex flex-col items-center justify-between gap-4 pt-10 pb-6 sm:flex-row sm:gap-0'>
         <div className='flex items-center  gap-6'>
           <a href='#' className='text-secondary-100 hover:text-white'>
             Privacy Policy
@@ -127,7 +131,9 @@ const Footer = (): JSX.Element => {
             Terms of Use
           </a>
         </div>
-        <div className='text-secondary-100'>contact@cascadia.foundation</div>
+        <div className='cursor-pointer text-secondary-100'>
+          contact@cascadia.foundation
+        </div>
       </div>
     </div>
   );
