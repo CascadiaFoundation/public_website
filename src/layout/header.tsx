a0940d4058eb8c988487d8279ff3f2ab4b321bce
Tony.W <credit.first000@gmail.com>
2022-09-02 09:50:43 -0500

    fix: remove user information from header dropdown menu

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,25 +1,11 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Disclosure } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import NavBtn, { btnProps } from '@/components/navBtn';
-
-type userProps = {
-  name: string;
-  email: string;
-  imageUrl: string;
-};
-
-const user: userProps = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
 
 const navigation: btnProps[] = [
   {
@@ -102,46 +88,6 @@ const Header = (): JSX.Element => {
                       <Link href={item.href}>{item.name}</Link>
                     </Disclosure.Button>
                   ))}
-                </div>
-                <div className='border-t border-secondary-200 pt-4 pb-3'>
-                  <div className='flex items-center px-5'>
-                    <div className='flex-shrink-0'>
-                      <Image
-                        width={40}
-                        height={40}
-                        className='rounded-full'
-                        src={user.imageUrl}
-                        alt=''
-                      />
-                    </div>
-                    <div className='ml-3'>
-                      <div className='text-base font-medium leading-none text-white'>
-                        {user.name}
-                      </div>
-                      <div className='text-sm font-medium leading-none text-secondary-100'>
-                        {user.email}
-                      </div>
-                    </div>
-                    <button
-                      type='button'
-                      className='ml-auto flex-shrink-0 rounded-full bg-white p-1 text-main-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                    >
-                      <span className='sr-only'>View notifications</span>
-                      <BellIcon className='h-6 w-6' aria-hidden='true' />
-                    </button>
-                  </div>
-                  {/* <div className='mt-3 space-y-1 px-2'>
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as='a'
-                        href={item.href}
-                        className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div> */}
                 </div>
               </Disclosure.Panel>
             </>
