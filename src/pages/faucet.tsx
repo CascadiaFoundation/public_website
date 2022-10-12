074b15ea7727879850fc78878cca1c120b605b75
phantom930 <phantom93930@gmail.com>
2022-10-13 01:39:35 +0900

    fix: remove-web3-connection

--- src/pages/faucet.tsx
+++ src/pages/faucet.tsx
@@ -1,23 +1,12 @@
-import { useWeb3React } from '@web3-react/core';
-import Cookies from 'js-cookie';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 import Btn from '@/components/btn';
 import toast from '@/components/toast';
 
-// import Web3Status from '@/components/web3Status';
-import { ChainId } from '@/config/chainIds';
-import { network } from '@/config/wallets';
-import { NetworkContextName } from '@/constants';
 import Layout from '@/layout';
-import { useActiveWeb3React } from '@/services/web3';
-
 const Faucet = (): JSX.Element => {
-  const { account, deactivate } = useActiveWeb3React();
-  const { activate: activateNetwork, active: networkActive } =
-    useWeb3React(NetworkContextName);
   const [inputAddress, setInputAddress] = useState<string>('');
   const [checkedAddress, setCheckedAddress] = useState<boolean>(false);
 
@@ -47,25 +36,6 @@ const Faucet = (): JSX.Element => {
       });
   }, [checkedAddress, inputAddress, notify]);
 
-  useEffect(() => {
-    if (account) {
-      setInputAddress(account);
-      setCheckedAddress(true);
-    } else {
-      setInputAddress('');
-      setCheckedAddress(false);
-    }
-  }, [account]);
-
-  useEffect(() => {
-    //add and switch chain automatically
-    if (networkActive) return;
-    deactivate();
-    Cookies.set('chain-id', ChainId.FUSE.toString());
-    network.changeChainId(ChainId.FUSE);
-    activateNetwork(network);
-  }, [activateNetwork, deactivate, networkActive]);
-
   return (
     <Layout>
       <div className='m-auto p-4 md:px-10 md:py-6 lg:px-32 lg:py-14'>
@@ -75,9 +45,6 @@ const Faucet = (): JSX.Element => {
               <h2 className='mt-5 w-full text-center text-2xl font-semibold text-primary-900 md:mt-0 md:text-left'>
                 Cascadia Faucet
               </h2>
-              {/* <div className='mt-5 mr-5 flex w-full items-center justify-end md:mt-0 md:mr-0'>
-                <Web3Status />
-              </div> */}
             </div>
             <div className='mt-8 w-full max-w-[990px] font-normal text-primary-900'>
               {`Our Cascadia faucet distributes tasks and rewards in tiny, micro-sized bits to encourage our community to visit this more frequently. You can currently request up to 10 test coin for Cascadia through our faucet program once every hour. It is supported by donations, so if you have any extra tokens, please think about giving them to yet another fellow "testnetter".`}
