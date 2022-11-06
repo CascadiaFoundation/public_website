de6dea4dfd7d168ac2e8aded343db0e72be7d009
Dev_Star <phantom93930@gmail.com>
2022-11-06 12:44:49 +0900

    fix: change-montserrat-font-style-loading-page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -71,7 +71,9 @@ function MyApp({ Component, pageProps }) {
         </>
       ) : (
         <div className='m-auto flex min-h-screen w-full items-center justify-center'>
-          <div className='text-5xl font-bold text-main-900'>Cascadia</div>
+          <div className='font-mont text-5xl font-bold text-main-900'>
+            Cascadia
+          </div>
         </div>
       )}
     </>
